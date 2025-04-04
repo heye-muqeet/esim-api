032dd28d8af33dd1e9767dac65ce254720bf2944
Abdul Muqeet <engr.abdulmuqeet42501@gmail.com>
2025-04-04 20:14:17 +0500

    feat: enhance AuthService with user validation and structured response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,18 +6,28 @@ import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { User } from '../user/user.entity';
 
+type Response = {
+  success: boolean;
+  message: string;
+  user: {
+    id: number;
+    email: string;
+  };
+  accessToken: string;
+};
+
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
     private jwtService: JwtService,
-  ) {}
+  ) { }
 
   // Add this method
   async validateUser(email: string, password: string): Promise<User | null> {
     const user = await this.usersRepository.findOne({ where: { email } });
-    
+
     if (!user) {
       return null;
     }
@@ -26,22 +36,43 @@ export class AuthService {
     return isPasswordValid ? user : null;
   }
 
-  async signUp(email: string, password: string): Promise<{ accessToken: string }> {
+  async signUp(email: string, password: string): Promise<Response> {
     const existingUser = await this.usersRepository.findOne({ where: { email } });
-    
+
     if (existingUser) {
       throw new ConflictException('Email already exists');
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = this.usersRepository.create({ email, password: hashedPassword });
-    await this.usersRepository.save(user);
-    
-    return this.generateToken(user.id);
+    const newUser = await this.usersRepository.save(user);
+
+    const accessToken = this.generateToken(newUser.id);
+
+    return {
+      success: true,
+      message: 'User registered successfully',
+      user: {
+        id: newUser.id,
+        email: newUser.email,
+      },
+      accessToken: accessToken.accessToken,
+    }
   }
 
-  async login(user: User): Promise<{ accessToken: string }> {
-    return this.generateToken(user.id);
+  async login(user: User): Promise<Response> {
+    console.log('loging in user', user);
+    const accessToken = this.generateToken(user.id);
+
+    return {
+      success: true,
+      message: 'User registered successfully',
+      user: {
+        id: user.id,
+        email: user.email,
+      },
+      accessToken: accessToken.accessToken,
+    }
   }
 
   private generateToken(userId: number) {
